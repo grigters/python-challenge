cb2a421413b98a05fc5c3a44b79c95b0a5f63e1d
grigters <grigters@gmail.com>
2019-02-09 16:19:28 -0600

    updated files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,26 +1,79 @@
+#import libararies
 import os
 import csv
 
-csvpath = os.path.join('Resources', 'budget_data.csv')
+#create variables
+total_months = 0
+Profit_Loss = 0
+num = 0
+average_change = 0
+pl_list = []
+new = []
+date = []
+
+#-------------------
+csvpath = os.path.join('Resources','budget_data.csv')
 
 with open(csvpath, newline='') as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
 
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
+    
     for row in csvreader:
-        print(row)
+        #count total months
+        total_months += 1
+        #count total net profit/loss
+        Profit_Loss += int(row[1])
+        #put profit/loss into a list
+        pl_list.append(row[1])
+        #put date into a list
+        date.append(row[0])
 
-#-------------------
-# Set variable for output file
-output_file = os.path.join("Resources", "output.csv")
+  
+for x in pl_list:
+    #keep num <= to 84 to stay in list range
+    if num <= 84:
+        #check change by month and add to average_change
+        check =  - int(pl_list[num]) + int(pl_list[(num+1)])
+        average_change += check
+        num += 1 
+        #append averages by month to list
+        new.append(check)
 
-#  Open the output file
-with open(output_file, "w", newline="") as datafile:
-    writer = csv.writer(datafile)
+#round to two decimals
+average_round = round((average_change/num), 2)
 
-    # Write the header row
-    writer.writerow(["Title", "Course Price", "Subscribers", "Reviews Left",
-                     "Percent of Reviews", "Length of Course"])
+#Get the max and min
+#print(max(new))
+#print(min(new))
+
+#now find the index for max and min numbers
+#print(new.index(1926159))
+#print(new.index(-2196167))
+
+#find the corresponding dates and add 1, because the first cell is blank for new list
+#print(date[(24+1)])
+#print(date[(43+1)])
+
+#print to terminal
+print(f'Financial Analysis')
+print(f'-----------------------------------')
+print(f'Total Month: {total_months}')
+print(f'Total: ${Profit_Loss}')   
+print(f'Average Change: ${average_round}') 
+print(f'Greatest Increase in Profits: {date[(24+1)]} (${max(new)})')
+print(f'Greatest Decrease in Profits: {date[(43+1)]} (${min(new)})')
+
+#------------------- 
+file = 'output.txt'
+
+with open(file, 'w') as text:
+
+    text.write(f'Financial Analysis\n')
+    text.write(f'-----------------------------------\n')
+    text.write(f'Total Month: {total_months}\n')
+    text.write(f'Total: ${Profit_Loss}\n')
+    text.write(f'Average Change: ${average_round}\n')
+    text.write(f'Greatest Increase in Profits: {date[(24+1)]} (${max(new)})\n')
+    text.write(f'Greatest Decrease in Profits: {date[(43+1)]} (${min(new)})')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,16 +1,97 @@
+#import libraries
 import os
 import csv
 
-csvpath = os.path.join('..', 'Resources', 'accounting.csv')
+#create variables for the candidates after finding unique candidates (see unique_list)
+khan = 0
+correy = 0
+li = 0
+otooley = 0
+
+#-1 for num so header is not counted
+num = -1
+unique_list = [] 
+
+csvpath = os.path.join('Resources', 'election_data.csv')
 
 with open(csvpath, newline='') as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
-
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
     for row in csvreader:
-        print(row)
+        #get total votes
+        num += 1
+        if row[2] not in unique_list: 
+            unique_list.append(row[2])
+        
+        #after locating unique candidates, count their votes
+        if row[2] == 'Khan':
+            khan += 1
+        elif row[2] == 'Correy':
+            correy += 1
+        elif row[2] == 'Li':
+            li += 1
+        elif row[2] == "O'Tooley":
+            otooley += 1
+  
+#------------------- 
+file = 'output.txt'
+
+with open(file, 'w') as text:
+
+    print('Election Results')
+    print('------------------------')
+    print(f'Total Votes: {num}')
+    print('------------------------')
+    text.write('Election Results\n')
+    text.write('------------------------\n')
+    text.write(f'Total Votes: {num}\n')
+    text.write('------------------------\n')
+    
+    #calculate the percentage of votes by candidate
+    khan_pct = round(((khan/num)*100),3)
+    correy_pct = round(((correy/num)*100),3)
+    li_pct = round(((li/num)*100),3)
+    otooley_pct = round(((otooley/num)*100),3)
+
+    #remove index 0 which contains the headet
+    unique_list.pop(0)
+
+    #create a dictionary to making printing out the results easier
+    kha_dict = {'name': unique_list[0],'percent': khan_pct, 'votes': khan}
+    cor_dict = {'name': unique_list[1],'percent': correy_pct, 'votes': correy}
+    li_dict = {'name': unique_list[2],'percent': li_pct, 'votes': li}
+    oto_dict = {'name': unique_list[3],'percent': otooley_pct, 'votes': otooley}
+
+    #print and write to terminal and text file
+    print(f"{kha_dict['name']}: {kha_dict['percent']}% ({kha_dict['votes']})")
+    print(f"{cor_dict['name']}: {cor_dict['percent']}% ({cor_dict['votes']})")
+    print(f"{li_dict['name']}: {li_dict['percent']}% ({li_dict['votes']})")
+    print(f"{oto_dict['name']}: {oto_dict['percent']}% ({oto_dict['votes']})")
+    text.write(f"{kha_dict['name']}: {kha_dict['percent']}% ({kha_dict['votes']})\n")
+    text.write(f"{cor_dict['name']}: {cor_dict['percent']}% ({cor_dict['votes']})\n")
+    text.write(f"{li_dict['name']}: {li_dict['percent']}% ({li_dict['votes']})\n")
+    text.write(f"{oto_dict['name']}: {oto_dict['percent']}% ({oto_dict['votes']})\n")
+
+    #create list to find winner
+    winner = [khan, correy, li, otooley]
+
+    print('------------------------')
+    text.write('------------------------\n')
+
+    #if statement to find the winner by popular vote
+    if max(winner) == kha_dict['votes']:
+        print('Winner: Khan')
+        text.write('Winner: Khan\n')
+    elif max(winner) == cor_dict['votes']:
+        print('Winner: Correy')
+        text.write('Winner: Correy\n')
+    elif max(winner) == li_dict['votes']:
+        print('Winner: Li')
+        text.write('Winner: Li\n')
+    else:
+        print("Winner: O'Tooley")
+        text.write("Winner: O'Tooley\n")
+    
+    print('------------------------')
+    text.write('------------------------\n')
